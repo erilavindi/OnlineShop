5993c95e6fb0ce1d798595efbed6cb5bf0d5f366
unknown <erielilavindi@gmail.com>
2023-05-13 21:02:03 +0700

    new laravel project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Home;
+use App\Http\Requests\StoreHomeRequest;
+use App\Http\Requests\UpdateHomeRequest;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $viewData = [];
+        $viewData["title"] = "Home Page - Online Store";
+        return view('home.index')->with("viewData", $viewData);
+    }
+
+    public function about()
+    {
+        $viewData = [];
+        $viewData["title"] = "About us - Online Store";
+        $viewData["subtitle"] = "About us";
+        $viewData["description"] = "This is an about page ...";
+        $viewData["author"] = "Developed by: Your Name";
+        return view('home.about')->with("viewData", $viewData);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('title', 'Home Page - Online Store') 
+@section('subtitle',"Subtitlenya diganti")
+
+@section('content')
+<div class="text-center">
+    Welcome to the application
+</div>
+@endsection
